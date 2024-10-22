4230a3b2426f9dc7529528614f40ff26aa092671
CoderPradeep307028 <pradeep2003kumar2002@gmail.com>
2024-10-23 00:28:49 +0530

    initial commit

---
+++ card07/src/components/data.jsx
@@ -0,0 +1,75 @@
+const data = [
+    {
+        id: 1,
+        name: "Jaipur",
+        info: " The city of Jaipur is the capital of Rajasthan state in northwestern India. Located in the east-central part of the state, it is by far Rajasthan's largest city. Jaipur is sometimes called the “pink city” because many of its buildings are rose colored. Jaipur is a walled city almost completely surrounded by hills.",
+        image: "https://media.istockphoto.com/id/1345573278/photo/hawa-mahal-in-jaipur.jpg?s=612x612&w=0&k=20&c=yTdArufMV3F0LkGJkafm4QpG3ln_lCQD97f3zG7mL2c=",
+        price: "12000",
+    },
+
+    {
+        id: 2,
+        name: "Jaisalmar",
+        info: " The city of Jaisalmer also acts as the guard to western Rajasthan (and India's) frontier. This 'Golden City' is located close to the Pakistan border and in close proximity to the Thar Desert. The city's most prominent landmark is the Jaisalmer Fort, also called Sonar Qila (Golden Fort). ",
+        image: "https://media.istockphoto.com/id/695001168/photo/city-view-with-jaisalmer-fort.jpg?s=612x612&w=0&k=20&c=-AjdZoDTu_i9rXDj5STdsn6K1Ml3w1YYmR9qAVm6wUs=",
+        price: "15000",
+    },
+
+    {
+        id: 3,
+        name: "Udaipur",
+        info: " Udaipur is a popular tourist destination and is known for its history, culture, scenic locations and the Rajput-era palaces. It is popularly known as the City of Lakes because of its sophisticated lake system. It has seven lakes surrounding the city.",
+        image: "https://media.istockphoto.com/id/484762648/photo/city-palace-udaipur.jpg?s=612x612&w=0&k=20&c=fCUt8-ooqPA3IhtY2_DDfluVceTOein22y4TMFnqHeM=",
+        price: "10000",
+    },
+
+    {
+        id: 4,
+        name: "Jodhpur",
+        info: " Jodhpur is a famous tourist spot with a palace, fort, and temples, set in the stark landscape of the Thar Desert. It's also known as the Blue City due to the dominant color scheme of its buildings in old town. The old city circles the Mehrangarh Fort and is bounded by a wall with several gates.",
+        image: "https://media.istockphoto.com/id/1091257902/photo/palace-in-golden-charm.jpg?s=612x612&w=0&k=20&c=oa9JD7BCvKskAWzCahj2Wstii71iDs-GzbDr-4rk8ew=",
+        price: "8000",
+    },
+
+    {
+        id: 5,
+        name: "Pushkar",
+        info: " Pushkar, town, central Rajasthan state, northwestern India. It lies in the Aravalli Range about 6 miles (10 km) northwest of Ajmer. The town is a pilgrimage centre that contains five temples, all of modern construction because the earlier buildings were destroyed by the Mughal emperor Aurangzeb (reigned 1658–1707).",
+        image: "https://media.istockphoto.com/id/2171870344/photo/ghats-at-pushkar-lake-in-india.jpg?s=612x612&w=0&k=20&c=pAqpIElqIv5_8Ls94E-PHly2Shyid32s4jhm-KngdaE=",
+        price: "5000",
+    },
+
+    {
+        id: 6,
+        name: "Bhilwara",
+        info: "It lies in an upland region about 30 miles (48 km) north of Chittaurgarh. Bhilwara was formerly a part of Udaipur princely state, and it became part of the state of Rajasthan in 1948. The city is a rail and road communications hub as well as a market centre.",
+        image: "https://media.istockphoto.com/id/1354379785/photo/beautiful-architecture-of-gurudwara-in-amritsar.jpg?s=612x612&w=0&k=20&c=884-kBcT_AwNQypBXP8b9Wh3yKj9C7GPW6KxK-z2cUE=",
+        price: "3000",
+    },
+
+    {
+        id: 7,
+        name: "Abu Road",
+        info: " Abu Road is an important Industrial and commercial city in the southwestern region of Rajasthan State on the bank of West Banas river. City is surrounded by Aravali hill range and about 482 kms from state capital Jaipur and about 248 kms from Jodhpur City.",
+        image: "https://media.istockphoto.com/id/1206472415/photo/toad-rock-in-mount-abu-india.jpg?s=612x612&w=0&k=20&c=U2YgMLCYL6PjxRLeEzLc-3v8MyBdFJz2-OksJnBedzY=",
+        price: "5000",
+    },
+    
+    {
+        id: 8,
+        name: " Bundi",
+        info: "In southern Rajasthan, Bundi exudes historical charm with its well-preserved architecture and captivating tales of the bygone era. This quaint town, often overshadowed by its more prominent counterparts, offers a serene escape for history enthusiasts and culture seekers.",
+        image: "https://www.rajasthandriver.com/img/travel-info/667x445_city-bundi.jpg",
+        price: "5000",
+    },
+    
+    {
+        id: 9,
+        name: "Bikaner",
+        info: "Situated in the heart of the Thar Desert, Bikaner stands as a testament to the grandeur of Rajasthan. This desert jewel boasts a rich cultural heritage, magnificent forts, and palaces that narrate tales of valour and royalty.A day in Bikaner allows you to delve into its regal history, exploring the architectural marvels and immersing yourself in the cultural richness that defines this desert city.",
+        image: "https://www.rajasthandriver.com/img/travel-info/667x445_city-bikaner.jpg",
+        price: "5000",
+    },
+];
+
+export default data;
